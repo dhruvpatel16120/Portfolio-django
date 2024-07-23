15e429647b60c0c7482ee33f3cecfd8866436d56
Dhruv PATEL <143148503+dhruvpatel16120@users.noreply.github.com>
2024-07-23 14:09:23 +0530

    Update settings.py

--- DPO/settings.py
+++ DPO/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware', 
+
 ]
 
 ROOT_URLCONF = 'DPO.urls'
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # add manually
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # setup media dir
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
