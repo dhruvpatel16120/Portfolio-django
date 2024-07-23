185f649542aea1f4323c5194af0334c14ec8b7f3
Dhruv PATEL <143148503+dhruvpatel16120@users.noreply.github.com>
2024-07-23 13:51:45 +0530

    Update wsgi.py

--- DPO/wsgi.py
+++ DPO/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=BASE_DIR + "static/")
+application = WhiteNoise(application, root=BASE_DIR + "/static/")
 app = application #add here
