8470644cabe3bc9b533cf693a8791d91d8ee66dc
Dhruv PATEL <143148503+dhruvpatel16120@users.noreply.github.com>
2024-07-26 10:06:16 +0530

    add privous js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,6 +1,16 @@
+/**
+* Template Name: iPortfolio
+* Updated: Nov 17 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
+  /**
+   * Easy selector helper function
+   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -10,117 +20,9 @@
     }
   }
 
-  
-  particlesJS("hero", {
-  "particles": {
-    "number": {
-      "value": 250,
-      "density": {
-        "enable": true,
-        "value_area": 900
-      }
-    },
-    "color": {
-      "value": "#2261c7"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 1.05,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#2261c7",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-  
+  /**
+   * Easy event listener function
+   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -132,10 +34,16 @@
     }
   }
 
+  /**
+   * Easy on scroll event listener 
+   */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
+  /**
+   * Navbar links active state on scroll
+   */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -153,6 +61,9 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
+  /**
+   * Scrolls to an element with header offset
+   */
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop
     window.scrollTo({
@@ -161,6 +72,9 @@
     })
   }
 
+  /**
+   * Back to top button
+   */
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -174,12 +88,18 @@
     onscroll(document, toggleBacktotop)
   }
 
+  /**
+   * Mobile nav toggle
+   */
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -195,6 +115,9 @@
     }
   }, true)
 
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -203,6 +126,9 @@
     }
   });
 
+  /**
+   * Hero type effect
+   */
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
@@ -216,6 +142,9 @@
     });
   }
 
+  /**
+   * Skills animation
+   */
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -230,6 +159,9 @@
     })
   }
 
+  /**
+   * Porfolio isotope and filter
+   */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -257,10 +189,16 @@
 
   });
 
+  /**
+   * Initiate certificate lightbox 
+   */
   const certificateLightbox = GLightbox({
     selector: '.certificate-lightbox'
   });
 
+  /**
+   * certificate isotope and filter
+   */
   window.addEventListener('load', () => {
     let certificateContainer = select('.certificate-container');
     if (certificateContainer) {
@@ -288,7 +226,17 @@
 
   });
 
-  const portfolioDetailsSlider = new Swiper('.portfolio-details-slider', {
+  /**
+   * Initiate certificate lightbox 
+   */
+  const CertificateLightbox = GLightbox({
+    selector: '.certificate-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
     autoplay: {
@@ -302,7 +250,10 @@
     }
   });
 
-  const testimonialsSlider = new Swiper('.testimonials-slider', {
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
     autoplay: {
@@ -320,6 +271,7 @@
         slidesPerView: 1,
         spaceBetween: 20
       },
+
       1200: {
         slidesPerView: 3,
         spaceBetween: 20
@@ -327,6 +279,9 @@
     }
   });
 
+  /**
+   * Animation on scroll
+   */
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -336,7 +291,9 @@
     })
   });
 
+  /**
+   * Initiate Pure Counter 
+   */
   new PureCounter();
 
-})();
-  
+})()
