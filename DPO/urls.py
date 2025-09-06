5e68fb5b99a908f078edf6992999ec8d6549d42b
Dhruv PATEL <143148503+dhruvpatel16120@users.noreply.github.com>
2025-09-06 13:09:43 +0530

    Update urls.py

--- DPO/urls.py
+++ DPO/urls.py
@@ -42,6 +42,6 @@ urlpatterns = [
 ]
 handler404 = 'Home.views.handling_404'
 
-# Add URL patterns for serving media files during development.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
